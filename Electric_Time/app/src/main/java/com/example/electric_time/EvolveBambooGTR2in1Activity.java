a3b55b35834ac6babe5e3dd9775d075177dceb77
Areeb Khalfay <arkhalfay@yahoo.com>
2020-09-18 15:53:59 -0700

    All main functionality complete

--- Electric_Time/app/src/main/java/com/example/electric_time/EvolveBambooGTR2in1Activity.java
+++ Electric_Time/app/src/main/java/com/example/electric_time/EvolveBambooGTR2in1Activity.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EvolveBambooGTR2in1Activity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.evolveBambooGTR2in1Activity.input";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_evolve_bamboo_g_t_r2in1);
 
         Intent intent = getIntent();
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, EvolveBambooGTR2in1Activity2.class);
+        EditText editText = (EditText) findViewById(R.id.inputDistance);
+        String inputDistance = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, inputDistance);
+        startActivity(intent);
     }
 }
--- Electric_Time/app/src/main/java/com/example/electric_time/HovertraxHoverboardActivity.java
+++ Electric_Time/app/src/main/java/com/example/electric_time/HovertraxHoverboardActivity.java
@@ -4,14 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HovertraxHoverboardActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.hovertraxHoverboard.input";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hovertrax_hoverboard);
 
         Intent intent = getIntent();
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, HovertraxHoverboardActivity2.class);
+        EditText editText = (EditText) findViewById(R.id.inputDistance);
+        String inputDistance = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, inputDistance);
+        startActivity(intent);
     }
 }
